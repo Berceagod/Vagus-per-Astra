24992143e3a95094decb387e2baac009a77b6ab4
Bercea Mihnea-Cristian <mihnea910@gmail.com>
2024-10-31 15:32:37 +0200

    Commit tesst la SFML

--- main.cpp
+++ main.cpp
@@ -38,28 +38,8 @@ int main() {
     std::array<int, 100> v{};
     int nr;
     std::cout << "Introduceți nr: ";
-    /////////////////////////////////////////////////////////////////////////
-    /// Observație: dacă aveți nevoie să citiți date de intrare de la tastatură,
-    /// dați exemple de date de intrare folosind fișierul tastatura.txt
-    /// Trebuie să aveți în fișierul tastatura.txt suficiente date de intrare
-    /// (în formatul impus de voi) astfel încât execuția programului să se încheie.
-    /// De asemenea, trebuie să adăugați în acest fișier date de intrare
-    /// pentru cât mai multe ramuri de execuție.
-    /// Dorim să facem acest lucru pentru a automatiza testarea codului, fără să
-    /// mai pierdem timp de fiecare dată să introducem de la zero aceleași date de intrare.
-    ///
-    /// Pe GitHub Actions (bife), fișierul tastatura.txt este folosit
-    /// pentru a simula date introduse de la tastatură.
-    /// Bifele verifică dacă programul are erori de compilare, erori de memorie și memory leaks.
-    ///
-    /// Dacă nu puneți în tastatura.txt suficiente date de intrare, îmi rezerv dreptul să vă
-    /// testez codul cu ce date de intrare am chef și să nu pun notă dacă găsesc vreun bug.
-    /// Impun această cerință ca să învățați să faceți un demo și să arătați părțile din
-    /// program care merg (și să le evitați pe cele care nu merg).
-    ///
-    /////////////////////////////////////////////////////////////////////////
+
     std::cin >> nr;
-    /////////////////////////////////////////////////////////////////////////
     for(int i = 0; i < nr; ++i) {
         std::cout << "v[" << i << "] = ";
         std::cin >> v[i];
@@ -69,37 +49,22 @@ int main() {
     for(int i = 0; i < nr; ++i) {
         std::cout << "- " << v[i] << "\n";
     }
-    ///////////////////////////////////////////////////////////////////////////
-    /// Pentru date citite din fișier, NU folosiți tastatura.txt. Creați-vă voi
-    /// alt fișier propriu cu ce alt nume doriți.
-    /// Exemplu:
-    /// std::ifstream fis("date.txt");
-    /// for(int i = 0; i < nr2; ++i)
-    ///     fis >> v2[i];
-    ///
-    ///////////////////////////////////////////////////////////////////////////
-    ///                Exemplu de utilizare cod generat                     ///
-    ///////////////////////////////////////////////////////////////////////////
+
     Helper helper;
     helper.help();
-    ///////////////////////////////////////////////////////////////////////////
 
     SomeClass *c = getC();
     std::cout << c << "\n";
     delete c;
 
     sf::RenderWindow window;
-    ///////////////////////////////////////////////////////////////////////////
-    /// NOTE: sync with env variable APP_WINDOW from .github/workflows/cmake.yml:31
-    window.create(sf::VideoMode({800, 700}), "My Window", sf::Style::Default);
-    ///////////////////////////////////////////////////////////////////////////
-    //
-    ///////////////////////////////////////////////////////////////////////////
-    /// NOTE: mandatory use one of vsync or FPS limit (not both)            ///
-    /// This is needed so we do not burn the GPU                            ///
-    window.setVerticalSyncEnabled(true);                                    ///
-    /// window.setFramerateLimit(60);                                       ///
-    ///////////////////////////////////////////////////////////////////////////
+    window.create(sf::VideoMode({800, 700}), "Testare sfml sa vedem WINDOW", sf::Style::Default);
+    window.setVerticalSyncEnabled(true);
+
+    // Creează un cerc alb
+    sf::CircleShape circle(50); // 50 este raza cercului
+    circle.setFillColor(sf::Color::White); // Setează culoarea cercului
+    circle.setPosition(375, 325); // Poziția (x, y) pentru centrul cercului
 
     while(window.isOpen()) {
         bool shouldExit = false;
@@ -130,6 +95,10 @@ int main() {
         std::this_thread::sleep_for(300ms);
 
         window.clear();
+
+        // Desenează cercul
+        window.draw(circle);
+
         window.display();
     }
     return 0;
