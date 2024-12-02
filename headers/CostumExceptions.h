f9be8f1ec269f152dea8a681adb81a00731e71bf
Berceagod <102349260+Berceagod@users.noreply.github.com>
2024-12-02 22:23:11 +0200

    v0.31

---
+++ headers/CostumExceptions.h
@@ -0,0 +1,15 @@
+//
+// Created by mihne on 12/2/2024.
+//
+
+#ifndef COSTUMEXCEPTIONS_H
+#define COSTUMEXCEPTIONS_H
+#include <exception>
+
+// class AbilityAlreadyThere : public std::exception {
+// public:
+//     [[nodiscard]] const char* what() const noexcept override;
+// };
+
+
+#endif //COSTUMEXCEPTIONS_H
